2c926bcea254070a69a4ce2b2e741436d5e70eaf
Dracarys <laurine.sorel@free.fr>
2019-04-08 18:09:55 +0200

    Api component created hiking / extract / geolocalize / utm  routes add

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbTabChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+import { environment } from './../../environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+  })
+};
 
 @Component({
   selector: 'app-api',
@@ -7,9 +18,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiComponent implements OnInit {
 
-  constructor() { }
+  public title = "Projet ICC 2k19"
+  public backEndUrl = environment.backEndUrl;
+  public isCollapsed = true;
+
+  // Lien vers l'api scala entré par l'utilisateur
+  public apiLink = "/hiking/921410";
+  // Json response
+  public jsonResponse = null;
+  // Body bodyRequest
+  public bodyRequest = "";
+  public body = false;
+
+  editorOptions = {
+    theme: 'vs-dark',
+    language: 'json',
+    minimap: {
+      enabled: false
+    },
+    wordWrap: "on"
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  getApiLink(ngbTabChangeEvent: NgbTabChangeEvent){
+    this.apiLink = ngbTabChangeEvent.nextId;
+    this.body = !(this.apiLink == "/hiking/921410");
+    this.jsonResponse = null;
+    this.bodyRequest = null;
+  }
+
+  onSubmit(){
+    if (this.body) {
+      this.httpClient.post(environment.backEndUrl + this.apiLink, this.bodyRequest, httpOptions).subscribe(
+        res => {
+          this.jsonResponse = res;
+        },
+        error => this.jsonResponse = { "error": error.error }
+      );
+    } else {
+      this.httpClient.get(environment.backEndUrl + this.apiLink).subscribe(
+        res => {
+          this.jsonResponse = res;
+        },
+        error => this.jsonResponse = { "error": error.error }
+      );
+    }
+  }
+
+  copyToClipBoard(){
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+    dummy.setAttribute("id", "dummy_id");
+    dummy.value = JSON.stringify(this.jsonResponse, null, 4);
+    dummy.select();
+    document.execCommand("copy");
+    document.body.removeChild(dummy);
+  }
 
   ngOnInit() {
+    this.onSubmit();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NgxJsonViewerModule } from 'ngx-json-viewer';
+import { HttpClientModule } from '@angular/common/http';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 
 import { AppComponent } from './app.component';
 import { ApiComponent } from './api/api.component';
@@ -15,7 +19,11 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    NgxJsonViewerModule,
+    HttpClientModule,
+    MonacoEditorModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
