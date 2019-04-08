3d02123d10c7db8319bf1c6e5ae17e1107ccdc15
Dracarys <laurine.sorel@free.fr>
2019-04-08 10:26:28 +0200

    Initialization of angular project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ApiComponent }      from './api/api.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/api', pathMatch: 'full' },
+  { path: 'api', component: ApiComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { ApiComponent } from './api/api.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ApiComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
